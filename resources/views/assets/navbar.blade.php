e0033d5d9bbd78b2b2a656e92b973e0c7d2392e4
Didarul Karim Jewel <m.diarulkarimjewel@gmail.com>
2019-09-16 20:22:34 +0600

    basic UI ready with admin lte

---
+++ resources/views/assets/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="" class="nav-link">Home</a>
+        </li>
+    </ul>
+
+</nav>
---
+++ resources/views/assets/sidebar.blade.php
@@ -0,0 +1,31 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+    <!-- Brand Logo -->
+    <a href="" class="brand-link">
+        <img style="margin-left: 50px">
+        <span class="brand-text font-weight-light">Laravel CRUD</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                data-accordion="false">
+
+                <li class="nav-item " >
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fa  fa-university"></i>
+                        <p>
+                           Manage User
+                        </p>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+
+    </div>
+
+</aside>
